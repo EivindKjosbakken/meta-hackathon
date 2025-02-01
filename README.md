fcd835f479c66bbbe36b669495b78987dc564f5a
Eivind Kjosbakken <eivindkjosbakken@gmail.com>
2025-02-01 12:42:56 +0100

    add nebius inference

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Llama3.3 Hackathon
+
+
+## Team members
+
+- https://github.com/Jesper-Hustad
+- https://github.com/sveinjhu
+- https://github.com/Zoroa1885
+- https://github.com/EivindKjosbakken
+
---
+++ nebius_inference.py
@@ -0,0 +1,29 @@
+from os import environ
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+api_key = environ.get("NEBIUS_API_KEY")
+temperature = environ.get("TEMPERATURE", 0.1)
+
+MODEL = "meta-llama/Llama-3.3-70B-Instruct"
+
+assert api_key is not None, "NEBIUS_API_KEY is not set"
+assert temperature is not None, "TEMPERATURE is not set"
+
+client = OpenAI(
+    base_url="https://api.studio.nebius.ai/v1/",
+    api_key=api_key,
+)
+
+def inference(prompt: str) -> str:
+    completion = client.chat.completions.create(
+        model=MODEL,
+        messages=[{"role": "user", "content": prompt}],
+        temperature=float(temperature),
+    )
+    return completion.choices[0].message.content
+
+
+inference("hi, how are you?")
