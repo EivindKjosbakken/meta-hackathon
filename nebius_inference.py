b7df1c61d866bafb54ac0ce8a5653e3322d0c7e5
Eivind Kjosbakken <eivindkjosbakken@gmail.com>
2025-02-01 17:44:40 +0100

    use faster llama 3.3 model

--- nebius_inference.py
+++ nebius_inference.py
@@ -7,8 +7,8 @@ load_dotenv()
 api_key = environ.get("NEBIUS_API_KEY")
 temperature = environ.get("TEMPERATURE", 0.1)
 
-MODEL = "meta-llama/Llama-3.3-70B-Instruct"
-# MODEL = "meta-llama/Llama-3.3-70B-Instruct-fast"
+# MODEL = "meta-llama/Llama-3.3-70B-Instruct"
+MODEL = "meta-llama/Llama-3.3-70B-Instruct-fast"
 
 assert api_key is not None, "NEBIUS_API_KEY is not set"
 assert temperature is not None, "TEMPERATURE is not set"
